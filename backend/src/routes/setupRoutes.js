715b2dfa96ad4dc91516a870312d77942a4b4968
Armando Rios <armandorios.dev@gmail.com>
2025-04-08 10:41:57 -0700

    chore: remove legacy JavaScript structure

--- backend/src/routes/setupRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/src/routes/setupRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createInitialAdmin } = require('../controllers/setupController');
-
-router.post('/initial-admin', createInitialAdmin);
-
-module.exports = router;
