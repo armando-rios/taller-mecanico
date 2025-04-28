d4110676c0d0d241ab3568f58e524de7143f4ef7
Armando Rios <armandorios.dev@gmail.com>
2025-04-27 19:17:16 -0700

    feat: use created routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import * as dotenv from "dotenv"
 import connectDB from "./config/db"
+import authRoutes from "./routes/auth.routes"
 
 dotenv.config()
 const port = process.env.PORT
@@ -12,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 
 connectDB()
+
+app.use('/api/auth', authRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
