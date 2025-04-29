6cdd3ea398198acf7d0dd8182e0bc0b4d6eb5c3a
Armando Rios <armandorios.dev@gmail.com>
2025-04-28 20:13:02 -0700

    refactor: user IUser interface

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -1,46 +1,51 @@
-import jwt from "jsonwebtoken"
-import User from "../models/User.model"
-import { Request, Response } from "express"
+import jwt from 'jsonwebtoken';
+import User from '../models/User.model';
+import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
+import { IUser } from '../interfaces/user.interface';
 
-const generateToken = (id: any) => {
+interface RequestWithUser extends Request {
+  user?: IUser;
+}
+
+const generateToken = (id: ObjectId) => {
   if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined")
+    throw new Error('JWT_SECRET is not defined');
   }
 
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  })
-}
+    expiresIn: '30d',
+  });
+};
 
 export const register = async (req: Request, res: Response) => {
-
   try {
-    const { name, username, password, rol } = req.body
+    const { name, username, password, rol } = req.body;
 
-    const userExists = await User.findOne({ username })
+    const userExists = await User.findOne({ username });
     if (userExists) {
       res.status(400).json({
-        message: "The user already exists"
-      })
-      throw new Error("The user already exists");
+        message: 'The user already exists',
+      });
+      throw new Error('The user already exists');
     }
 
-    if (rol && !["admin", "tecnico", "vendedor"].includes(rol)) {
+    if (rol && !['admin', 'tecnico', 'vendedor'].includes(rol)) {
       res.status(400).json({
-        message: "Invalid rol"
-      })
-      throw new Error("Invalid rol");
+        message: 'Invalid rol',
+      });
+      throw new Error('Invalid rol');
     }
 
     const user = await User.create({
       name,
       username,
       password,
-      rol
-    })
+      rol,
+    });
 
     if (!user) {
-      throw new Error("User not created")
+      throw new Error('User not created');
     }
 
     return res.status(201).json({
@@ -48,29 +53,29 @@ export const register = async (req: Request, res: Response) => {
       name: user.name,
       username: user.username,
       rol: user.rol,
-      token: generateToken(user._id)
-    })
-  } catch (error: any) {
+      token: generateToken(user._id),
+    });
+  } catch (error) {
     return res.status(500).json({
-      message: error.message
-    })
+      message: (error as Error).message,
+    });
   }
-}
+};
 
 export const login = async (req: Request, res: Response) => {
   try {
-    const { username, password } = req.body
+    const { username, password } = req.body;
 
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
 
     if (!user) {
-      throw new Error("User not found")
+      throw new Error('User not found');
     }
 
-    const isMatch = await user.comparePassword(password)
+    const isMatch = await user.comparePassword(password);
 
     if (!isMatch) {
-      throw new Error("Invalid password")
+      throw new Error('Invalid password');
     }
 
     return res.status(200).json({
@@ -78,27 +83,29 @@ export const login = async (req: Request, res: Response) => {
       name: user.name,
       username: user.username,
       rol: user.rol,
-      token: generateToken(user._id)
-    })
-  } catch (error: any) {
+      token: generateToken(user._id),
+    });
+  } catch (error) {
     return res.status(500).json({
-      message: error.message
-    })
+      message: (error as Error).message,
+    });
   }
-}
+};
 
-export const getUsers = async (req: Request, res: Response) => {
+export const getUsers = async (req: RequestWithUser, res: Response) => {
   try {
-    const users = await User.find()
+    const users = await User.find();
 
     if (!users) {
-      throw new Error("Users not found")
+      throw new Error('Users not found');
     }
 
-    return res.status(200).json(users)
-  } catch (error: any) {
+    console.log(req.user);
+
+    return res.status(200).json(users);
+  } catch (error) {
     return res.status(500).json({
-      message: error.message
-    })
+      message: (error as Error).message,
+    });
   }
-}
+};
--- apps/backend/src/middleware/auth.middleware.ts
+++ apps/backend/src/middleware/auth.middleware.ts
@@ -1,27 +1,29 @@
-import jwt, { JwtPayload } from "jsonwebtoken"
-import User from "../models/User.model"
-import { Request, Response, NextFunction } from "express"
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import User from '../models/User.model';
+import { Request, Response, NextFunction } from 'express';
+import { IUser } from '../interfaces/user.interface';
 
 interface RequestWithUser extends Request {
-  user?: any
+  user?: IUser;
 }
 
 export const protect = async (req: RequestWithUser, res: Response, next: NextFunction) => {
-  let token: string
+  let token: string;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      token = req.headers.authorization.split(" ")[1]
+      token = req.headers.authorization.split(' ')[1];
       if (!token) {
-        res.status(401)
-        throw new Error("Not authorized, no token")
+        res.status(401);
+        throw new Error('Not authorized, no token');
       }
-      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload
-      req.user = await User.findById(decoded.id).select("-password")
-      next()
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
     } catch (error) {
-      res.status(401)
-      throw new Error("Not authorized, token failed")
+      res.status(401).json({
+        message: (error as Error).message,
+      });
     }
   }
-}
+};
