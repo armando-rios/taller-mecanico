89653863f3eb0aba1da03a3952713727a17d6921
Armando Rios <armandorios.dev@gmail.com>
2025-04-28 20:13:33 -0700

    refactor: change IUser to interface

--- apps/backend/src/models/User.model.ts
+++ apps/backend/src/models/User.model.ts
@@ -1,55 +1,49 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model } from 'mongoose';
 import bcrypt from 'bcrypt';
+import { IUser } from '../interfaces/user.interface';
 
-interface IUser extends Document {
-  name: string;
-  username: string;
-  password: string;
-  rol: 'admin' | 'tecnico' | 'vendedor';
-  createdAt: Date;
-  updatedAt: Date;
-  comparePassword(enteredPassword: string): Promise<boolean>
-}
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Por favor ingrese un nombre'],
+    },
+    username: {
+      type: String,
+      required: [true, 'Por favor ingrese un nombre de usuario'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Por favor ingrese una contraseña'],
+    },
+    rol: {
+      type: String,
+      enum: ['admin', 'tecnico', 'vendedor'],
+      default: 'vendedor',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const userSchema = new Schema<IUser>({
-  name: {
-    type: String,
-    required: [true, 'Por favor ingrese un nombre']
-  },
-  username: {
-    type: String,
-    required: [true, 'Por favor ingrese un nombre de usuario'],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Por favor ingrese una contraseña'],
-  },
-  rol: {
-    type: String,
-    enum: ['admin', 'tecnico', 'vendedor'],
-    default: 'vendedor'
-  }
-}, {
-  timestamps: true
-});
-
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
   }
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next()
+    next();
   } catch (error) {
     next(error as Error);
   }
 });
 
-userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
+userSchema.methods.comparePassword = async function (enteredPassword: string): Promise<boolean> {
   return await bcrypt.compare(enteredPassword, this.password);
-}
+};
 
 const User = model<IUser>('User', userSchema);
 
