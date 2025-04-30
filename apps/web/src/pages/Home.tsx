8e56d6c3d0522b01872b4765a988ea618d7c6291
Armando Rios <armandorios.dev@gmail.com>
2025-04-30 08:22:07 -0700

    feat: add basic buttons

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,7 +1,14 @@
 const Home = () => {
   return (
     <>
-      <h1>Welcome to the Home Page!</h1>
+      <div className="flex gap-2">
+        <button className="py-2 px-4 bg-orange-700 rounded-lg font-semibold border-2 border-orange-700 hover:border-white">
+          Realizar Venta
+        </button>
+        <button className="py-2 px-4 border rounded-lg font-semibold hover:text-orange-700">
+          Registrar Servicio
+        </button>
+      </div>
     </>
   );
 };
