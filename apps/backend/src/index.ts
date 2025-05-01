4fda6ba3af1f7affbf37bebc53267c8ed05aa0e9
Armando Rios <armandorios.dev@gmail.com>
2025-04-30 19:14:23 -0700

    feat: add new routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 import connectDB from './config/db';
 import authRoutes from './routes/auth.routes';
+import partsRoutes from './routes/parts.routes';
 
 dotenv.config();
 const port = process.env.PORT;
@@ -15,6 +16,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/auth', authRoutes);
+app.use('/api', partsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
