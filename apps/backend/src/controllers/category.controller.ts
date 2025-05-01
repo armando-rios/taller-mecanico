2125b0fe8c7072bf0da1caa50d282cebb8b7e04f
Armando Rios <armandorios.dev@gmail.com>
2025-05-01 15:05:18 -0700

    feat: create category controller
    - getCategories
    - createCategory

---
+++ apps/backend/src/controllers/category.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import Category from '../models/Category.model';
+import { IUser } from '../interfaces/user.interface';
+
+interface RequestWithUser extends Request {
+  user?: IUser;
+}
+
+export const createCategory = async (req: Request, res: Response) => {
+  try {
+    const category = await Category.create(req.body);
+
+    if (!category) {
+      throw new Error('Category not created');
+    }
+
+    res.status(201).json(category);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+export const getCategories = async (req: RequestWithUser, res: Response) => {
+  try {
+    const categories = await Category.find();
+
+    if (!categories) {
+      throw new Error('Categories not found');
+    }
+
+    console.log({ userData: req.user, categorias: categories });
+
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
