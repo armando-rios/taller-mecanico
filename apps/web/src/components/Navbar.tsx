8a95faf0aec0baecac208a1a841a4fbf0d028a3e
Armando Rios <armandorios.dev@gmail.com>
2025-05-03 13:57:58 -0700

    style: add height size

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="flex items-center justify-between p-4">
+    <header className="h-1/14 flex items-center justify-between px-4">
       <h1 className="text-2xl font-bold text-orange-600">Taller Mecanico</h1>
       <nav className="flex gap-4 items-center">
         {navigation.map((item) => (
