8a4feecf50144c2c6b015cc5ce2749e1024380d5
Armando Rios <armandorios.dev@gmail.com>
2025-05-03 13:58:40 -0700

    feat: use Serach component

--- apps/web/src/pages/Invetory.tsx
+++ apps/web/src/pages/Invetory.tsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import api from "../config/axios";
 import Modal from "../components/inventory/Modal";
 import { Part, Category } from "../types/invetory";
+import Search from "../components/inventory/Search";
 
 const Invetory = () => {
-  const [parts, setParts] = useState([]);
+  const [parts, setParts] = useState<Part[]>([]);
+  const [filteredParts, setFilteredParts] = useState<Part[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [categories, setCategories] = useState<Category[]>([]);
 
@@ -22,6 +24,7 @@ const Invetory = () => {
       const { data } = await api.get("/parts");
       fetchCategories();
       setParts(data);
+      setFilteredParts(data);
     } catch (error) {
       console.log(error);
     }
@@ -30,11 +33,12 @@ const Invetory = () => {
   useEffect(() => {
     fetchCategories();
     fetchParts();
+    setFilteredParts(parts);
   }, []);
 
   return (
     <>
-      <div className="p-4 bg-neutral-800 rounded-lg gap-4 flex flex-col">
+      <div className="p-4 bg-neutral-800 rounded-lg gap-4 flex flex-col flex-1 h-full">
         <div className="flex items-center justify-between">
           <h2 className="text-2xl font-bold">Inventario</h2>
           <button
@@ -49,50 +53,53 @@ const Invetory = () => {
         {isModalOpen && (
           <Modal setIsModalOpen={setIsModalOpen} fetchParts={fetchParts} />
         )}
-        <div className="overflow-x-auto">
-          <table className="w-full bg-neutral-700 rounded-lg overflow-hidden">
-            <thead className="bg-neutral-600">
-              <tr>
-                <th className="p-3 text-left">Código</th>
-                <th className="p-3 text-left">Producto</th>
-                <th className="p-3 text-left">Categoría</th>
-                <th className="p-3 text-left">Stock</th>
-                <th className="p-3 text-left">Precio</th>
-                <th className="p-3 text-left">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {parts.map((part: Part) => {
-                return (
-                  <tr
-                    className="border-t border-gray-700 hover:bg-gray-700"
-                    key={part._id}
-                  >
-                    <td className="p-3">{part.code}</td>
-                    <td className="p-3">{part.name}</td>
-                    <td className="p-3">
-                      {
-                        categories.find(
-                          (category: Category) =>
-                            category._id === part.category,
-                        )?.name
-                      }
-                    </td>
-                    <td className="p-3">{part.stock}</td>
-                    <td className="p-3">{part.price}</td>
-                    <td className="p-3 flex space-x-2">
-                      <button className="text-orange-700 hover:underline">
-                        Editar
-                      </button>
-                      <button className="text-red-500 hover:underline">
-                        Eliminar
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+        <Search parts={parts} setFilteredParts={setFilteredParts} />
+        <div className="flex-1 overflow-hidden flex flex-col min-h-0">
+          <div className="flex-1 overflow-y-auto rounded-lg">
+            <table className="w-full bg-neutral-700 rounded-lg overflow-hidden">
+              <thead className="bg-neutral-600 sticky top-0">
+                <tr>
+                  <th className="p-3 text-left">Código</th>
+                  <th className="p-3 text-left">Producto</th>
+                  <th className="p-3 text-left">Categoría</th>
+                  <th className="p-3 text-left">Stock</th>
+                  <th className="p-3 text-left">Precio</th>
+                  <th className="p-3 text-left">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredParts.map((part: Part) => {
+                  return (
+                    <tr
+                      className="border-t border-gray-700 hover:bg-gray-700"
+                      key={part._id}
+                    >
+                      <td className="p-3 w-1/6">{part.code}</td>
+                      <td className="p-3">{part.name}</td>
+                      <td className="p-3">
+                        {
+                          categories.find(
+                            (category: Category) =>
+                              category._id === part.category,
+                          )?.name
+                        }
+                      </td>
+                      <td className="p-3">{part.stock}</td>
+                      <td className="p-3">{part.price}</td>
+                      <td className="p-3 flex space-x-2">
+                        <button className="text-orange-700 hover:underline">
+                          Editar
+                        </button>
+                        <button className="text-red-500 hover:underline">
+                          Eliminar
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
