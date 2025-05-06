0517608fb01a23047d9ede7e9ef6eb9afd81943f
Armando Rios <armandorios.dev@gmail.com>
2025-05-06 14:52:43 -0700

    feat: change component to use real data and add basic funcionality
    - Add props for use and set data
    - add click function to set data on SaleCart

--- apps/web/src/components/home/Product.tsx
+++ apps/web/src/components/home/Product.tsx
@@ -1,12 +1,36 @@
-const Product = () => {
+import { Part } from "../../types/invetory";
+
+interface ProductProps {
+  data: Part;
+  setPartsList: React.Dispatch<React.SetStateAction<Part[]>>;
+}
+
+const Product = ({ data, setPartsList }: ProductProps) => {
+  const handleAddToCart = () => {
+    setPartsList((prevParts) => {
+      const existingPartIndex = prevParts.findIndex((p) => p._id === data._id);
+
+      if (existingPartIndex >= 0) {
+        return prevParts.map((part, index) =>
+          index === existingPartIndex ? { ...part } : part,
+        );
+      } else {
+        return [...prevParts, { ...data }];
+      }
+    });
+  };
+
   return (
-    <div className="bg-neutral-700 p-3 rounded-lg hover:bg-neutral-600 cursor-pointer">
+    <div
+      className="bg-neutral-700 p-3 rounded-lg hover:bg-neutral-600 cursor-pointer"
+      onClick={handleAddToCart}
+    >
       <div className="flex justify-between">
-        <span className="font-medium">Filtro de Aceite</span>
-        <span className="text-orange-700">$12.99</span>
+        <span className="font-medium">{data.name}</span>
+        <span className="text-orange-700">${data.price}</span>
       </div>
       <div className="text-xs text-gray-400 mt-1">
-        Cód: REP-00123 | Stock: 15
+        Cód: {data.code} | Stock: {data.stock}
       </div>
     </div>
   );
