3bbfa36bb45cd9cb8af7dff8520a561acb70c9c0
Armando Rios <armandorios.dev@gmail.com>
2025-05-06 14:57:30 -0700

    feat: get data for use on SaleModal Component and add button function

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
+import MakeSaleModal from "../components/home/MakeSaleModal";
+import api from "../config/axios";
+import { Part } from "../types/invetory";
+
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [parts, setParts] = useState<Part[]>([]);
+
+  const fetchParts = async () => {
+    try {
+      const { data } = await api.get("/parts");
+      setParts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchParts();
+  }, []);
+
   return (
     <>
+      {isModalOpen && <MakeSaleModal parts={parts} />}
       <div className="flex gap-2">
-        <button className="py-2 px-4 bg-orange-700 rounded-lg font-semibold border-2 border-orange-700 hover:border-white">
+        <button
+          className="py-2 px-4 bg-orange-700 rounded-lg font-semibold border-2 border-orange-700 hover:border-white"
+          onClick={() => setIsModalOpen(true)}
+        >
           Realizar Venta
         </button>
         <button className="py-2 px-4 border rounded-lg font-semibold hover:text-orange-700">
